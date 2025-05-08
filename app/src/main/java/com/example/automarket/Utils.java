dbe3d940f8cb49593fde3eef93a6a16ee2ef793b
nataliaamartinez <nataliamartinezluna17@gmail.com>
2025-05-08 19:51:02 +0200

    punto en el que se empieza de cero(aqui funciona todo)

--- app/src/main/java/com/example/automarket/Utils.java
+++ app/src/main/java/com/example/automarket/Utils.java
@@ -1,5 +1,5 @@
 package com.example.automarket;
 
 public class Utils {
-    public static final  String IP = "http://192.168.1.47/automarket/";
+    public static final  String IP = "http://192.168.56.1/automarket/";
 }
